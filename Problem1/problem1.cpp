179d63c2814266bccf18281cfd3151e29060ce4b
Ryan MacLeod <rkmacleod17@gmail.com>
2022-05-02 21:16:27 -0400

    Project Euler Problems 1-3
    Solved with brute force methods

---
+++ Problem1/problem1.cpp
@@ -0,0 +1,18 @@
+//Problem #1: Multiples of 3 or 5
+//Description: If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Output: Find the sum of all the multiples of 3 or 5 below 1000.
+
+#include <iostream>
+
+int main()
+{
+    int sum = 0;        //int for sum of all multiples of 3 or 5 below 1000
+    
+    for(int i = 0; i < 1000; i++)
+    {
+        if((i % 3 == 0) || (i % 5 == 0))
+            sum += i;
+    }
+    std::cout << sum << std::endl;
+    return sum;
+}
---
+++ Problem3/problem3.cpp
@@ -0,0 +1,22 @@
+//Problem #2: Largest prime factor
+//Description: The prime factors of 13195 are 5, 7, 13 and 29.
+//Output: What is the largest prime factor of the number 600851475143 ?
+
+#include <iostream>
+
+int main()
+{
+    const long long PRIME_NUM = 600851475143;       //Large number being factored
+    long long temp = PRIME_NUM;                     //used for storing number being factored
+    long long factor = 1;                           //used for factoring large number
+
+    while(temp != 1)
+    {
+        factor++;
+        if(temp % factor == 0)
+            temp /= factor;
+    }
+
+    std::cout << factor << std::endl;
+    return 0;
+}
